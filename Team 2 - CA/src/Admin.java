1bd5e3d6de02eda7279ca3b06490ac93093c9505
FawazAli1 <fawaz.ali@city.ac.uk>
2026-03-21 18:00:58 +0000

    Created role specific classes

--- Team 2 - CA/src/Admin.java
+++ Team 2 - CA/src/Admin.java
@@ -1,2 +1,8 @@
-public class Admin {
+//Admin is an extension of the parent class User
+public class Admin extends User {
+    //constructor for Admin and takes username, password and name as input
+    public Admin(String username, String password, String name) {
+        //calls the constructor of the parent class
+        super(username, password, "Admin", name);
+    }
 }
